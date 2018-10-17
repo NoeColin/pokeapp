ae7e7de9f963137e766025b159c4a5a3e97f56fa
Noé Colin <noe_colin@yahoo.fr>
2018-10-17 11:17:27 +0200

    login button implement

--- app/src/main/java/com/example/lpiem/pokeapp/MainActivity.java
+++ app/src/main/java/com/example/lpiem/pokeapp/MainActivity.java
@@ -3,11 +3,24 @@ package com.example.lpiem.pokeapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private LoginResult loginResult;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        callbackManager = CallbackManager.Factory.create();
     }
+
+
+
 }
