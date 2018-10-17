84023c33e871ff987ef4ee00c4597454fe667339
Noé Colin <noe_colin@yahoo.fr>
2018-10-17 14:07:50 +0200

    fb login

--- app/src/main/java/com/example/lpiem/pokeapp/MainActivity.java
+++ app/src/main/java/com/example/lpiem/pokeapp/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.lpiem.pokeapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
@@ -13,12 +18,56 @@ public class MainActivity extends AppCompatActivity {
 
     private LoginResult loginResult;
     private CallbackManager callbackManager;
+    private LoginButton loginButton;
+    private Context context;
+
+    int duration = Toast.LENGTH_SHORT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         callbackManager = CallbackManager.Factory.create();
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+
+        context = getApplicationContext();
+        // If using in a fragment
+      //  loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                Toast.makeText(MainActivity.this, "good", Toast.LENGTH_SHORT).show();
+
+
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(MainActivity.this, "cancel", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Toast.makeText(MainActivity.this, "nop", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 
